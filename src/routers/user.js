614e358fddd12e15653c9375de1afd6da7ce1d30
Jean Broqua <js.broqua@gmail.com>
2022-01-03 16:45:46 -0300

    added sharp moduled and setup image formating and resize for the avatar picture

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
+const sharp = require('sharp')
 const multer = require('multer')
 const router = express.Router()
 
@@ -107,13 +108,13 @@ router.post(
   auth,
   upload.single('avatar'),
   async (req, res) => {
-    try {
-      req.user.avatar = req.file.buffer
-      await req.user.save()
-      res.send()
-    } catch (error) {
-      console.log(error)
-    }
+    const buffer = await sharp(req.file.buffer)
+      .resize({ width: 250, height: 250 })
+      .png()
+      .toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message })
@@ -124,6 +125,21 @@ router.delete('/users/me/avatar', auth, async (req, res) => {
   req.user.avatar = undefined
   await req.user.save()
   res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+
+    if (!user || !user.avatar) {
+      throw new Error()
+    }
+
+    res.set('Content-Type', 'image/png')
+    res.send(user.avatar)
+  } catch (error) {
+    res.status(404).send()
+  }
 })
 
 module.exports = router
